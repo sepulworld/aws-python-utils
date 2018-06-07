0dbd4e292d852ebe608eda0ee7a265158016ef86
ctimmins <ctimmins@trulia.com>
2018-06-07 12:27:56 -0700

    fix

--- test/test_dp.py
+++ test/test_dp.py
@@ -1,4 +1,18 @@
+import unittest
+from unittest.mock import MagicMock
 from aws_python_utils.datapipeline import datapipeline
 
-if __name__ == '__main__':
-    datapipeline.get_by_name()
+
+class TestDataPipelineUtil(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_get_name(self):
+        pipelines = datapipeline.get_by_name()
+        self.assertIsNotNone(pipelines, "pipelines were not found")
+
+    if __name__ == '__main__':
+        unittest.main()
